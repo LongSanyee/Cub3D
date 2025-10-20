ccb6a98c3216bd3848ff241e8633e6891484f789
Reda Ammisse <rammisse@e1r5p12.1337.ma>
2025-10-20 20:25:18 +0100

    dd

--- render.c
+++ render.c
@@ -6,7 +6,7 @@
 /*   By: rammisse <rammisse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 00:06:10 by azaimi            #+#    #+#             */
-/*   Updated: 2025/10/20 19:48:34 by rammisse         ###   ########.fr       */
+/*   Updated: 2025/10/20 20:21:22 by rammisse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,8 @@ void	do_c_f(t_mlx *mlx)
 	t_color	color;
 
 	color.y = 0;
-	color.color = (mlx->data.floor[0] << 16) | (mlx->data.floor[1] << 8)
-		| mlx->data.floor[2];
+	color.color = (mlx->data.ceiling[0] << 16) | (mlx->data.ceiling[1] << 8)
+		| mlx->data.ceiling[2];
 	while (color.y < HEIGHT / 2)
 	{
 		color.x = 0;
@@ -45,8 +45,8 @@ void	do_c_f(t_mlx *mlx)
 		color.y++;
 	}
 	color.y = HEIGHT / 2;
-	color.color2 = (mlx->data.ceiling[0] << 16) | (mlx->data.ceiling[1] << 8)
-		| mlx->data.ceiling[2];
+	color.color2 = (mlx->data.floor[0] << 16) | (mlx->data.floor[1] << 8)
+		| mlx->data.floor[2];
 	while (color.y < HEIGHT)
 	{
 		color.x = 0;
@@ -57,6 +57,31 @@ void	do_c_f(t_mlx *mlx)
 		}
 		color.y++;
 	}
+}
+
+int	handlemouse(int x, int y, void *param)
+{
+	int			delta_x;
+	t_mlx		*win;
+	static int	prev_x = -1;
+
+	(void)y;
+	win = (t_mlx *)param;
+	delta_x = x - prev_x;
+	win->player.rotationangle += delta_x * 0.007;
+	prev_x = x;
+	return (0);
+}
+
+void	render(t_mlx *win)
+{
+	initstruct(&win->player);
+	mlx_hook(win->mlxwin, 17, 0, xbutton, win);
+	mlx_hook(win->mlxwin, 2, 1L << 0, keypress, win);
+	mlx_hook(win->mlxwin, 3, 1L << 1, keyrelease, win);
+	mlx_hook(win->mlxwin, 6, 1L << 6, handlemouse, win);
+	mlx_loop_hook(win->mlx, update, win);
+	mlx_loop(win->mlx);
 }
 
 void	render3dwalls(t_mlx *mlx)
@@ -85,29 +110,4 @@ void	render3dwalls(t_mlx *mlx)
 		drawtexture(mlx, &re);
 		re.i++;
 	}
-}
-
-int	handlemouse(int x, int y, void *param)
-{
-	int			delta_x;
-	t_mlx		*win;
-	static int	prev_x = -1;
-
-	(void)y;
-	win = (t_mlx *)param;
-	delta_x = x - prev_x;
-	win->player.rotationangle += delta_x * 0.007;
-	prev_x = x;
-	return (0);
-}
-
-void	render(t_mlx *win)
-{
-	initstruct(&win->player);
-	mlx_hook(win->mlxwin, 17, 0, xbutton, win);
-	mlx_hook(win->mlxwin, 2, 1L << 0, keypress, win);
-	mlx_hook(win->mlxwin, 3, 1L << 1, keyrelease, win);
-	mlx_hook(win->mlxwin, 6, 1L << 6, handlemouse, win);
-	mlx_loop_hook(win->mlx, update, win);
-	mlx_loop(win->mlx);
 }
